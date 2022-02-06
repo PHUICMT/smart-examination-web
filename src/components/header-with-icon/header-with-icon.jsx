17d5f5ff111d739f5d2e9408dca689eb20ab2057
Newiihuhu <ninewii_huhu@hotmail.com>
2022-02-06 18:55:27 +0700

    :cat: del log

--- src/components/header-with-icon/header-with-icon.jsx
+++ src/components/header-with-icon/header-with-icon.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { Container } from 'react-bulma-components';
 
 const HeaderWithIcon = (props) => {
-    console.log(props.icon)
     return (
         <React.Fragment>
             <Container>

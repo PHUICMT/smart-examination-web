ed5a798a37590642e387fd83da131592ecb25c35
sukorn <83692433+sukorn@users.noreply.github.com>
2022-03-17 02:04:12 +0700

    card-input Checkbox

--- src/components/info-card/info-card.jsx
+++ src/components/info-card/info-card.jsx
@@ -1,25 +1,29 @@
 import "./info-card.scss";
 
-import { Container } from 'react-bulma-components';
+import { Container } from "react-bulma-components";
 
 const InfoCard = (props) => {
+  let marginTop = props.marginTop;
+  let title = props.title;
+  let description = props.description;
+  let input = props.input;
 
-    let marginTop = props.marginTop
-    let title = props.title
-    let description = props.description
-    let radio = props.radio
-    return (
-        <Container>
-            <div className="info-card" style={{ marginTop: `${marginTop}px` }}>
-                <div className="info-card-text">
-                    <div className="info-card-text title-1">{title}</div>
-                    {description !== null ? <div className="info-card-text title-2">{description}</div> : null}
-                    {radio !== null ? <div className="info-card-text title-3">{radio}</div> : null}
-                </div>
-                <img src={props.icon} className="info-card-icon" />
-            </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <div className="info-card" style={{ marginTop: `${marginTop}px` }}>
+        <div className="info-card-text">
+          <div className="info-card-text title-1">{title}</div>
+          {description !== null ? (
+            <div className="info-card-text title-2">{description}</div>
+          ) : null}
+          {input !== null ? (
+            <div className="info-card-text title-3">{input}</div>
+          ) : null}
+        </div>
+        <img alt="icon info" src={props.icon} className="info-card-icon" />
+      </div>
+    </Container>
+  );
 };
 
 export default InfoCard;
--- src/pages/ExamPage/ExamPage.jsx
+++ src/pages/ExamPage/ExamPage.jsx
@@ -1,55 +1,96 @@
 import "./ExamPage.scss";
-import 'bulma';
+import "bulma";
 
-import React from 'react';
-import InfoCard from "../../components/info-card/info-card"
+import React from "react";
+import InfoCard from "../../components/info-card/info-card";
+import { Checkbox, FormGroup } from "@mui/material/";
+
 import {
-    RadioGroup,
-    FormControlLabel,
-    FormControl,
-    Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  Radio,
 } from "@material-ui/core";
 
 const ExamPage = () => {
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
+  return (
+    <React.Fragment>
+      <InfoCard
+        className="exam-card"
+        title="1. ขั้นตอนการพัฒนาอัลกอริทึมเพื่อแก้ปัญหา (Develops the algorithm for solution)หมายถึงข้อใด"
+        description={null}
+        icon={null}
+        marginTop={100}
+        input={
+          <FormControl className="radio-group">
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                value="anserOne"
+                control={<Radio />}
+                label="เอกสารประกอบโปรแกรมจะช่วยอธิบายถึงจุดประสงค์ของโปรแกรม"
+              />
+              <FormControlLabel
+                value="anserTwo"
+                control={<Radio />}
+                label="เอกสารประกอบโปรแกรมจะช่วยอธิบายถึงจุดประสงค์ของโปรแกรม"
+                color="success"
+              />
+              <FormControlLabel
+                value="anserThree"
+                control={<Radio />}
+                label="อัลกอริทึมในที่นี้หมายถึงสูตรทางคณิตศาสตร์ที่ใช้สำหรับในการแก้ปัญญา"
+              />
+              <FormControlLabel
+                value="anserFour"
+                control={<Radio />}
+                label="การวางแผนการเขียนโปรแกรม ด้วยการออกแบบให้มีเวลาการประมวลผล คือผังงาน(Flowchart)"
+              />
+            </RadioGroup>
+          </FormControl>
+        }
+      />
 
-    return (
-        <React.Fragment>
+      <InfoCard
+        className="exam-card"
+        title="2. เขียนโค้ดที่แสดงถึง MVC"
+        description={
+          <form>
+            <div className="form-group">
+              <textarea
+                class="textarea has-fixed-size"
+                placeholder="เขียนคำตอบ"
+              ></textarea>
+            </div>
+          </form>
+        }
+        input={null}
+        icon={null}
+        marginTop={100}
+      />
 
-            <InfoCard className="exam-card"
-                title="1. ขั้นตอนการพัฒนาอัลกอริทึมเพื่อแก้ปัญหา (Develops the algorithm for solution)หมายถึงข้อใด"
-                description={null}
-                icon={null}
-                marginTop={100}
-                radio={
-                    <FormControl className="radio-group" >
-                        <RadioGroup
-                            aria-labelledby="demo-radio-buttons-group-label"
-                            name="radio-buttons-group"
-                        >
-                            <FormControlLabel value="anserOne" control={<Radio />} label="เอกสารประกอบโปรแกรมจะช่วยอธิบายถึงจุดประสงค์ของโปรแกรม" />
-                            <FormControlLabel value="anserTwo" control={<Radio />} label="เอกสารประกอบโปรแกรมจะช่วยอธิบายถึงจุดประสงค์ของโปรแกรม"color="success"  />
-                            <FormControlLabel value="anserThree" control={<Radio />} label="อัลกอริทึมในที่นี้หมายถึงสูตรทางคณิตศาสตร์ที่ใช้สำหรับในการแก้ปัญญา" />
-                            <FormControlLabel value="anserFour" control={<Radio />} label="การวางแผนการเขียนโปรแกรม ด้วยการออกแบบให้มีเวลาการประมวลผล คือผังงาน(Flowchart)" />
-                        </RadioGroup>
-                    </FormControl>
-                }
-            />
-
-            <InfoCard className="exam-card"
-                title="2. เขียนโค้ดที่แสดงถึง MVC"
-                description={
-                    <form>
-                        <div className="form-group" >
-                        <textarea class="textarea has-fixed-size" placeholder="เขียนคำตอบ"></textarea>
-                        </div>
-                    </form>
-                }
-                radio={null}
-                icon={null}
-                marginTop={100}
-            />
-        </React.Fragment>
-    );
+      <InfoCard
+        className="exam-card"
+        title="3. ข้อใดบ้างที่เกี่ยวของกับ React"
+        description={null}
+        icon={null}
+        marginTop={100}
+        input={
+          <div>
+            <FormGroup>
+              <FormControlLabel control={<Checkbox />} label="Javascript" />
+              <FormControlLabel control={<Checkbox />} label="Fontend" />
+              <FormControlLabel control={<Checkbox />} label="Library" />
+              <FormControlLabel control={<Checkbox />} label="Client" />
+            </FormGroup>
+          </div>
+        }
+      />
+    </React.Fragment>
+  );
 };
 
 export default ExamPage;

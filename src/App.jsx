ae352e86d7a57cf3c55eacc68b9eeb5c73be14a2
Newiihuhu <Ninewii_huhu@hotmail.com>
2022-03-20 06:23:49 +0700

    ⭐️ Route to Create Exam

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import Header from "./components/header/header";
 
-import LandingPage from './pages/LandingPage/LandingPage';
-import DashBoard from './pages/DashBoard/DashBoard';
-import EnterPin from './pages/EnterPin/EnterPin';
-import ExamPage from './pages/ExamPage/ExamPage';
+import LandingPage from "./pages/LandingPage/LandingPage";
+import DashBoard from "./pages/DashBoard/DashBoard";
+import EnterPin from "./pages/EnterPin/EnterPin";
+import CreateExam from "./pages/CreateExam/CreateExam";
+import ExamPage from "./pages/ExamPage/ExamPage";
 
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
-}
+};
 
 function App() {
   return (
@@ -25,8 +26,9 @@ function App() {
           <Route path="/student/enter-pin" component={EnterPin} />
           <Route path="/student/exampage" component={ExamPage} />
           <Route path="/teacher/dashboard" component={DashBoard} />
-          <Route path='*' exact >
-            <Redirect from='*' to='/index' />
+          <Route path="/teacher/create-exam" component={CreateExam} />
+          <Route path="*" exact>
+            <Redirect from="*" to="/index" />
           </Route>
         </Switch>
       </Router>

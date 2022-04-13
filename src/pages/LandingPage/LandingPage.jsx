c6e5cd7c73b3abb7c9075559a94ab4be4b62c40b
PHUICMT <PHUICMT@github.com>
2022-04-13 11:32:11 +0700

    🎨 Handle on user changed

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -10,7 +10,7 @@ import HeaderWithIcon from "../../components/header-with-icon/header-with-icon"
 import InfoCard from "../../components/info-card/info-card"
 import LoginPage from "../../components/login-page/login-page"
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Stack from '@mui/material/Stack';
 import PropTypes from 'prop-types';
 import Backdrop from '@mui/material/Backdrop';
@@ -18,13 +18,41 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import { useSpring, animated } from 'react-spring';
+import { useHistory, useLocation } from "react-router-dom";
 
 
 const LandingPage = () => {
-    const [isStudent, setIsStudent] = React.useState(true);
-    const [openLogin, setOpenLogin] = React.useState(false);
+    const history = useHistory();
+
+    const [userId, setUserId] = useState(null);
+    const [isStudent, setIsStudent] = useState(null);
+
+    const [openLogin, setOpenLogin] = useState(false);
+    const [loginAsStudent, setLoginAsStudent] = useState(false);
     const handleOpenLogin = () => setOpenLogin(true);
     const handleCloseLogin = () => setOpenLogin(false);
+
+    const [headerTitleCard, setHeaderTitleCard] = useState("เลือกกลุ่มผู้เข้าใช้ระบบ");
+    const [headerDescriptionCard, setHeaderDescriptionCard] = useState("โปรดเลือกประเภทสำหรับเข้าใช้ระบบจากปุ่มด้านล่าง");
+
+    useEffect(() => {
+        let sessionUserId = window.sessionStorage.getItem("userId");
+        let sessionName = window.sessionStorage.getItem("name");
+        let sessionIsStudent = window.sessionStorage.getItem("isStudent");
+
+        if (sessionUserId !== null && sessionName !== null && sessionIsStudent !== null) {
+            setUserId(sessionUserId);
+            setIsStudent(sessionIsStudent);
+
+            let title = "สวัสดีคุณ " + sessionName;
+            setHeaderTitleCard(title);
+
+            let description = "คุณเป็นผู้ใช้งานระบบสำหรับ " + (sessionIsStudent === "true" ? "นักเรียน" : "ครู") + " โปรดเลือกเมนูการทำงานต่อไป";
+            setHeaderDescriptionCard(description);
+        }
+    }, [userId, isStudent]);
+
+
 
     const Fade = React.forwardRef(function Fade(props, ref) {
         const { in: open, children, onEnter, onExited, ...other } = props;
@@ -82,13 +110,79 @@ const LandingPage = () => {
             >
                 <Fade in={openLogin}>
                     <Box sx={style}>
-                        <LoginPage isStudent={isStudent} />
+                        <LoginPage isStudent={loginAsStudent} />
                     </Box>
                 </Fade>
             </Modal>
         </div>)
     }
 
+    const ButtonGroupLogin = () => {
+        return (<div className="button-group">
+            <Stack direction="row" spacing={2}>
+                <Button
+                    variant="contained"
+                    className="student"
+                    onClick={() => {
+                        setLoginAsStudent(true)
+                        handleOpenLogin()
+                    }}
+                >
+                    <p>นักศึกษา</p>
+                    <img alt="studentIcon" src={studentIcon}></img>
+                </Button>
+                <Button
+                    variant="contained"
+                    className="teacher"
+                    onClick={() => {
+                        setLoginAsStudent(false)
+                        handleOpenLogin()
+                    }}
+                >
+                    <p>อาจารย์</p>
+                    <img alt="teacherIcon" src={teacherIcon}></img>
+                </Button>
+            </Stack>
+        </div>)
+    }
+
+    const ButtonGroupStudent = () => {
+
+        const gotoStudentExamPin = () => {
+            history.push("/student/enter-pin")
+        }
+
+        return (
+            <div className="button-group-student">
+                <Button
+                    variant="contained"
+                    onClick={gotoStudentExamPin}
+                >
+                    <p>เลือกแบบทดสอบ</p>
+                    <img alt="studentIcon" src={studentIcon}></img>
+                </Button>
+            </div>
+        )
+    }
+
+    const ButtonGroupTeacher = () => {
+
+        const gotoTeacherDashBoard = () => {
+            history.push("/teacher/dashboard")
+        }
+
+        return (
+            <div className="button-group-teacher">
+                <Button
+                    variant="contained"
+                    onClick={gotoTeacherDashBoard}
+                >
+                    <p>เข้าสู่หน้า Dashboard</p>
+                    <img alt="teacherIcon" src={teacherIcon}></img>
+                </Button>
+            </div>
+        )
+    }
     return (
         <React.Fragment>
             <HeaderWithIcon
@@ -97,37 +191,21 @@ const LandingPage = () => {
                 icon={silpakornIcon}
             />
             <InfoCard
-                title="เลือกกลุ่มผู้เข้าใช้ระบบ"
-                description="โปรดเลือกประเภทสำหรับเข้าใช้ระบบจากปุ่มด้านล่าง"
+                title={headerTitleCard}
+                description={headerDescriptionCard}
                 icon={maintenanceIcon}
             />
-            <div className="button-group">
-                <Stack direction="row" spacing={2}>
-                    <Button
-                        variant="contained"
-                        className="student"
-                        onClick={() => {
-                            setIsStudent(true)
-                            handleOpenLogin()
-                        }}
-                    >
-                        <p>นักศึกษา</p>
-                        <img alt="studentIcon" src={studentIcon}></img>
-                    </Button>
-                    <Button
-                        variant="contained"
-                        className="teacher"
-                        onClick={() => {
-                            setIsStudent(false)
-                            handleOpenLogin()
-                        }}
-                    >
-                        <p>อาจารย์</p>
-                        <img alt="teacherIcon" src={teacherIcon}></img>
-                    </Button>
-                </Stack>
-            </div>
-            <ShowLoginModal />
+            {isStudent !== null ?
+                isStudent ?
+                    <ButtonGroupStudent />
+                    :
+                    <ButtonGroupTeacher />
+                :
+                <>
+                    <ShowLoginModal />
+                    <ButtonGroupLogin />
+                </>
+            }
         </React.Fragment>
     );
 };
